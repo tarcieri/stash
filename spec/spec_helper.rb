662cb111d25dee03854aa0546b1bfa9e5188afc3
Tony Arcieri <tony@medioh.com>
2010-11-23 19:47:58 -0700

    Initialize Stash in the RSpec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,8 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'stash'
 require 'rspec'
 require 'rspec/autorun'
+
+# Connect to Redis
+Stash.setup :default, :adapter => :redis,
+                      :host => '127.0.0.1',
+                      :port => 6379
