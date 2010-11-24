5b96319f96494239380412842bfaf73887362d5f
Tony Arcieri <tony@medioh.com>
2010-11-23 20:29:48 -0700

    Actually initialize the Redis adapter

--- lib/stash/redis_adapter.rb
+++ lib/stash/redis_adapter.rb
@@ -5,5 +5,21 @@ class Stash
   # Adapter for the Redis data structures server. 
   # See http://code.google.com/p/redis/
   class RedisAdapter
+    attr_reader :capabilities
+    
+    def initialize(config)
+      # Symbolize keys in config
+      config = config.inject({}) { |h, (k, v)| h[k.to_sym] = v; h }
+      
+      [:host, :port].each do |key|
+        raise ArgumentError, "missing redis configuration option: #{key}" unless config[key]
+      end
+      
+      redis = Redis.new config
+      redis = Redis::Namespace.new config[:namespace], :redis => redis if config[:namespace]
+      
+      @capabilities = [:string]
+      @redis = redis
+    end
   end
 end
