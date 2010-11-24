9b8c446220b22cd123891abf5b16d3a435c1afc8
Tony Arcieri <tony@medioh.com>
2010-11-23 21:38:11 -0700

    Support for retrieving a non-default Stash configuration and deletion support

--- lib/stash/class_methods.rb
+++ lib/stash/class_methods.rb
@@ -11,6 +11,7 @@ class Stash
       default[key] = value
     end
     
+    # Add a configuration entry to Stash
     def setup(label, configuration)
       @configurations ||= {}
       @configurations[label.to_sym] = configuration
@@ -21,12 +22,24 @@ class Stash
       end
     end
   
+    # Retrieve the default Stash configuration
     def default
       @default ||= begin
         raise ConfigError, "please configure me with Stash.setup" unless @configurations
         raise ConfigError, "no default configuration" unless @configurations[:default]
         new(@configurations[:default])
       end
+    end
+    
+    # Retrieve a particular Stash configuration
+    def configuration(key)
+      @configurations[key.to_sym]
+    end
+    
+    # Delete a key from the Stash
+    def delete(key)
+      default.delete(key)
+      true
     end
   end
   
