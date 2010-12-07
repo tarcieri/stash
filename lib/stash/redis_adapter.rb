ed7ace1dd314a1b74fdc6963a5fb5be48ebbfad6
Tony Arcieri <tony@medioh.com>
2010-12-07 00:09:31 -0700

    Use a thread per request (at least until a connection pool can be implemented)

--- lib/stash/redis_adapter.rb
+++ lib/stash/redis_adapter.rb
@@ -4,23 +4,32 @@ require 'redis-namespace'
 class Stash
   # Adapter for the Redis data structures server. 
   # See http://code.google.com/p/redis/
-  class RedisAdapter
+  class RedisAdapter    
     def initialize(config)
-      # Symbolize keys in config
-      config = config.inject({}) { |h, (k, v)| h[k.to_sym] = v; h }
+      # Convert config keys to symbols
+      config = symbolize_config(config)
       
       raise ArgumentError, "missing 'host' key" unless config[:host]
       config[:port] ||= 6379 # Default Redis port
       
-      redis = Redis.new config
-      redis = Redis::Namespace.new config[:namespace], :redis => redis if config[:namespace]
-            
-      @redis = redis
+      @config = config
+    end
+
+    # Obtain a connection to Redis
+    def connection
+      redis = Redis.new @config
+      redis = Redis::Namespace.new @config[:namespace], :redis => redis if @config[:namespace]
+      
+      begin
+        yield redis
+      ensure
+        redis.quit
+      end
     end
     
     # Set a given key within Redis
     def []=(key, value)
-      @redis.set key.to_s, value.to_s
+      connection { |redis| redis.set key.to_s, value.to_s }
     end
     
     # Retrieve a given key from Redis
@@ -35,91 +44,109 @@ class Stash
     
     # Retrieve the type for a given key
     def type(key)
-      @redis.type key.to_s
+      connection { |redis| redis.type key.to_s }
     end
     
     # Retrieve a key as a string
     def get(key)
-      @redis.get key.to_s
+      connection { |redis| redis.get key.to_s }
     end
     
     # Delete a key
     def delete(key)
-      @redis.del key.to_s
+      connection { |redis| redis.del key.to_s }
     end
     
     # Push an element onto a list
     def list_push(name, value, side)
-      case side
-      when :right
-        @redis.rpush name.to_s, value.to_s
-      when :left
-        @redis.lpush name.to_s, value.to_s
-      else raise ArgumentError, "left or right plztks"
+      connection do |redis|
+        case side
+        when :right
+          redis.rpush name.to_s, value.to_s
+        when :left
+          redis.lpush name.to_s, value.to_s
+        else raise ArgumentError, "left or right plztks"
+        end
       end
     end
         
     # Pop from a list
     def list_pop(name, side)
-      case side
-      when :right
-        @redis.rpop name.to_s
-      when :left
-        @redis.lpop name.to_s
-      else raise ArgumentError, "left or right plztks"
+      connection do |redis|
+        case side
+        when :right
+          redis.rpop name.to_s
+        when :left
+          redis.lpop name.to_s
+        else raise ArgumentError, "left or right plztks"
+        end
       end
     end
     
     # Blocking pop from a list
     def list_blocking_pop(name, side, timeout = nil)
-      timeout ||= 0
-      res = case side
-      when :left
-        @redis.blpop name, timeout
-      when :right
-        @redis.brpop name, timeout
-      else raise ArgumentError, "left or right plztks"
-      end
+      connection do |redis|
+        timeout ||= 0
+        res = case side
+        when :left
+          redis.blpop name, timeout
+        when :right
+          redis.brpop name, timeout
+        else raise ArgumentError, "left or right plztks"
+        end
       
-      return res[1] if res
-      raise Stash::TimeoutError, "request timed out"
+        return res[1] if res
+        raise Stash::TimeoutError, "request timed out"
+      end
     end
     
     # Retrieve the length of a list
     def list_length(name)
-      @redis.llen name.to_s
+      connection { |redis| redis.llen name.to_s }
     end
     
     # Retrieve the given range from a list
     def list_range(name, from, to)
-      @redis.lrange name.to_s, from, to
+      connection { |redis| redis.lrange name.to_s, from, to }
     end
     
     # Retrieve a value from a hash
     def hash_get(name, key)
-      res = @redis.hget name.to_s, key.to_s
+      res = connection { |redis| redis.hget name.to_s, key.to_s }
       return if res == ""
       res
     end
     
     # Store a value in a hash
     def hash_set(name, key, value)
-      @redis.hset name.to_s, key.to_s, value.to_s
+      connection { |redis| redis.hset name.to_s, key.to_s, value.to_s }
     end
     
     # Retrieve the contents of a hash as a Ruby hash
     def hash_value(name)
-      @redis.hgetall name.to_s
+      connection { |redis| redis.hgetall name.to_s }
     end
     
     # Delete an entry from a hash
     def hash_delete(name, key)
-      @redis.hdel name.to_s, key.to_s
+      connection { |redis| redis.hdel name.to_s, key.to_s }
     end
     
     # Return the length of a hash
     def hash_length(name)
-      @redis.hlen name.to_s
+      connection { |redis| redis.hlen name.to_s }
+    end
+    
+    #######
+    private
+    #######
+    
+    def symbolize_config(config)
+      new_config = {}
+      config.each do |k, v|
+        new_config[k.to_sym] = v
+      end
+      new_config
     end
   end
 end
